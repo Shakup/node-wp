4886b168cf4ad0e5a8a63643db83e61453b21681
Sébastien DECAMME <sebastien.decamme@gmail.com>
2017-09-01 23:22:23 +0200

    es5 => es6

--- test/index.js
+++ test/index.js
@@ -1,23 +1,6 @@
-'use strict'
+const NodeWP = require('../lib/node-wp')
+const WP = new NodeWP('http://shakup.net/wp-json/wp/v2')
 
-const 
-	NodeWP = require( '../index' )
-	, wp   = new NodeWP( 'http://wordpress.dev/wp-json/wp/v2' )
-
-
-function output (json) {
-	if ( !json.length ) {
-		console.log( 'No result' )
-		return
-	}
-	
-	json.forEach( (item) => console.log(item) )
-}
-
-wp
-	.posts({
-		page: 1,
-		per_page: 5
-	})
-	.then( output )
-	.catch( (err) => console.log('ERROR! ', err) )
+WP.query('/posts')
+    .then(data => console.log(data))
+    .catch(err => console.log(err.message))
